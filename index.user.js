6221c9d3e84991d6e22733fad620db7f264fd9d6
so1ve <nn_201312@163.com>
2023-07-24 21:10:34 +0800

    chore: update namespace

--- index.user.js
+++ index.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Fuck Elon Musk
-// @namespace    https://github.com/so1ve/fuck-elonmusk
+// @namespace    https://greasyfork.org/scripts/471597-elon-musk
 // @version      0.1.0
 // @description  Fuck Elon Musk! Let's bring the Twitter bird back.
 // @author       Ray (https://github.com/so1ve)
