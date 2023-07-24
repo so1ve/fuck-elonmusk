d2c6677f3cdad16a3e5b9868b54b96827b7120d2
so1ve <nn_201312@163.com>
2023-07-24 21:23:01 +0800

    chore: rename script

--- bump.mjs
+++ bump.mjs
@@ -1,9 +1,9 @@
 import fs from "node:fs/promises";
 
 const { version } = JSON.parse(await fs.readFile("package.json"));
-const content = await fs.readFile("index.user.js", "utf8");
+const content = await fs.readFile("script.user.js", "utf8");
 const newContent = content.replace(
 	/\/\/ @version\s+(?:\S.*)?$/m,
 	`// @version      ${version}`,
 );
-await fs.writeFile("index.user.js", newContent, "utf8");
+await fs.writeFile("script.user.js", newContent, "utf8");
