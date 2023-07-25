6ca2bbaaeb916392740c31ac0df8bfc7b118e44b
so1ve <nn_201312@163.com>
2023-07-26 01:47:10 +0800

    chore: release v0.4.0

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fuck Elon Musk
 // @namespace    https://greasyfork.org/scripts/471597-elon-musk
-// @version      0.3.1
+// @version      0.4.0
 // @description  Fuck Elon Musk! Let's bring the Twitter bird back.
 // @author       Ray (https://github.com/so1ve)
 // @license      MIT
